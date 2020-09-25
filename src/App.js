e1d1caa719afd2f063fff4dd84627d4f99aeee89
moussa <moussaibn0@gmail.com>
2020-09-25 15:38:42 +0200

    commit 54

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import './Person/Person.css'
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        persons: [
+            {name: "Moussa", age: 25},
+            {name: "Rama", age: 23},
+            {name: "Meissa", age: 27}
+        ],
+        showPersons: false
+    };
+
+    switchNameHandler = newName => {
+        //console.log('Was clicked !')
+        this.setState({
+            persons: [
+                {name: newName, age: 5},
+                {name: "Rama", age: 23},
+                {name: "Meissa", age: 27}
+            ]
+        })
+    }
+
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: "Moussa", age: 23},
+                {name: event.target.value, age: 5},
+                {name: "Meissa", age: 27}
+            ]
+        })
+    }
+
+    togglePersonsHandler = () => {
+        this.setState({
+            showPersons: !this.state.showPersons
+        })
+    }
+
+    render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+        return (
+            <div className="App">
+                <h1>Hi, I'm a react App</h1>
+                <p>This is really working</p>
+                <button style={style}
+                        onClick={this.togglePersonsHandler}>Switch person
+                </button>
+
+                {this.state.showPersons ?
+                    <div>
+                        <Person name={this.state.persons[0].name}
+                                age={this.state.persons[0].age}
+                        />
+                        <Person name={this.state.persons[1].name}
+                                age={this.state.persons[1].age}
+                                click={this.switchNameHandler.bind(this, "depuis un paragraphe")}
+                                change={this.nameChangeHandler}
+                        >Hobbies:
+                            Basketball </Person>
+                        <Person name={this.state.persons[2].name}
+                                age={this.state.persons[2].age}> </Person>
+                    </div> : null
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>Hello I'm {props.name} and I'm {props.age} years old</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name}/>
+        </div>
+    );
+}
+
+export default person;
