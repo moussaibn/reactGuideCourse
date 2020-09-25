f19391dc812cf6c79e161ad4736ee0a5ec96a3b9
moussa <moussaibn0@gmail.com>
2020-09-25 17:29:37 +0200

    commit 56

--- src/App.js
+++ src/App.js
@@ -52,17 +52,12 @@ class App extends Component {
         if (this.state.showPersons)
             persons = (
                 <div>
-                    <Person name={this.state.persons[0].name}
-                            age={this.state.persons[0].age}
-                    />
-                    <Person name={this.state.persons[1].name}
-                            age={this.state.persons[1].age}
-                            click={this.switchNameHandler.bind(this, "depuis un paragraphe")}
-                            change={this.nameChangeHandler}
-                    >Hobbies:
-                        Basketball </Person>
-                    <Person name={this.state.persons[2].name}
-                            age={this.state.persons[2].age}> </Person>
+                    {this.state.persons.map(person => {
+                        return <Person
+                            name={person.name}
+                            age={person.age}
+                        />
+                    })}
                 </div>
             );
         return (
