f51385d8e6aa19d9baaaa40fc56c992712ee0819
moussa <moussaibn0@gmail.com>
2020-09-26 22:33:51 +0200

    commit 68

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import './Person/Person.css'
+import Radium from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -43,7 +44,11 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover':{
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }
         };
         let persons = null;
         if (this.state.showPersons) {
@@ -61,6 +66,10 @@ class App extends Component {
                 </div>
             );
             style.backgroundColor = 'red';
+            style[':hover']={
+                backgroundColor: 'salmon',
+                    color: 'black'
+            }
         }
         const classes = []
         if (this.state.persons.length<=2)
@@ -83,4 +92,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Radium from "radium";
 const person = (props) => {
     return (
         <div className="Person">
@@ -10,4 +10,4 @@ const person = (props) => {
     );
 }
 
-export default person;
+export default Radium(person);
